8ded05b8c60e05516a06d134f757b669b7e876dc
Paquito <paco.pereira.garcia@hotmail.com>
2026-03-05 23:32:26 +0100

    [ADD] Basic: añadido el archivo 03

---
+++ Basic/03-datatypes.js
@@ -0,0 +1,14 @@
+// String
+let name = "Paquito"
+
+// Números
+let age = 26 // Entero
+let height = 1.83// Decimal
+
+// Booleanos
+let verdadero = true
+let falso = false
+
+// Undefined
+let loquesea
+console.log(loquesea) // undefined
