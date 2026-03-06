ad004a4dd0acafaf53ffecb8eb9e58083d16f88a
Paquito <paco.pereira.garcia@hotmail.com>
2026-03-06 13:59:17 +0100

    [FIX] Basic: modificación en archivo 03-...

--- Basic/03-datatypes.js
+++ Basic/03-datatypes.js
@@ -1,14 +1,35 @@
 // String
 let name = "Paquito"
-
+console.log("Variable name 'Paquito':", name)
 // Números
 let age = 26 // Entero
 let height = 1.83// Decimal
+console.log("Variable age '26':", age)
+console.log("Variable height '1.83':", height)
 
 // Booleanos
 let verdadero = true
 let falso = false
+console.log("Variable verdadero 'true':", verdadero)
+console.log("Variable falso 'false':", falso)
 
 // Undefined
 let loquesea
-console.log(loquesea) // undefined
+console.log("Variable loquesea 'undefined':", loquesea)
+
+// Null
+let nada = null
+console.log("Variable nada 'null':", nada)
+
+// Symbol
+let sym = Symbol("simbolo")
+console.log("Variable sym 'Symbol(\"simbolo\")':", sym)
+console.log("Descripción de sym:", sym.description)
+console.log("Tipo de sym:", typeof sym)
+console.log("Variable 'sym':", sym)
+
+// BigInt
+let bigInt = 1234567890123456789012345678901234567890n
+console.log("Variable bigInt: ", bigInt)
+let bigInt2 = BigInt("1234567890123456789012345678901234567890")
+console.log("Variable bigInt2: ", bigInt2)
